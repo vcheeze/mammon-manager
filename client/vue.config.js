2bef1335e3ea67eda66fab5f35c738b24e17af4b
Wei Chen <wchen165@me.ema.ad.pwcinternal.com>
2019-07-25 11:05:15 +0400

    added custom fonts

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     css: {
         loaderOptions: {
             sass: {
-                data: `@import "@/styles/_variables.scss";`
+                data: `@import "@/styles/_variables.scss"; @import "@/styles/_fonts.scss";`
             }
         }
     }
