c3ab4acbd55fbb9bc73f493e96daa0745004e7c3
Peter Chen <manchesterutd7@gmail.com>
2019-07-28 08:18:58 +0400

    modified tag controller

--- server/controllers/tagsController.js
+++ server/controllers/tagsController.js
@@ -4,6 +4,7 @@ const Category = require('../models/category');
 const getAllTags = (req, res) => {
     Tag
         .find({})
+        .populate('category')
         .then(doc => {
             res.status(200).send({
                 message: 'Successful: retrieved all Tags!',
@@ -24,49 +25,56 @@ const getTagByName = (req, res) => {
         .findOne({
             name: tagName
         })
-        .then(doc => {
-            res.status(200).send({
-                message: 'Successful: retrieved Tag by name!',
-                tag: doc
-            });
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err
-            });
+        .populate('category')
+        .exec((err, tag) => {
+            if (err) console.error(err);
+            console.log(`The category is ${tag.category.name}`);
         });
+        // .then(doc => {
+        //     res.status(200).send({
+        //         message: 'Successful: retrieved Tag by name!',
+        //         tag: doc
+        //     });
+        // })
+        // .catch(err => {
+        //     res.status(500).send({
+        //         message: err
+        //     });
+        // });
 }
 
 const createTag = (req, res) => {
     let categoryId;
     Category
         .findOne({
-            name: req.body.categoryName
+            name: req.body.category
         })
         .then(doc => {
-            categoryId = doc._id
+            // console.log(doc);
+            categoryId = doc._id;
+            
+            // create the Tag
+            let tag = new Tag({
+                name: req.body.name,
+                category: categoryId
+            });
+        
+            tag.save()
+                .then(doc => {
+                    res.status(200).send({
+                        message: 'Successful: created new Tag!',
+                        tag: doc
+                    })
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        message: err
+                    })
+                });
         })
         .catch(err => {
             // TODO consider creating new Category if it doesn't exist
-            console.log(err)
-        });
-
-    let tag = new Tag({
-        name: req.body.name,
-        category: categoryId
-    });
-
-    tag.save()
-        .then(doc => {
-            res.status(200).send({
-                message: 'Successful: created new Tag!',
-                tag: doc
-            })
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err
-            })
+            console.log(err);
         });
 }
 
