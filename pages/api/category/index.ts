2982f41d6dd717cd0871401b904b09f505953882
Peter Chen <manchesterutd7@gmail.com>
2022-04-20 23:31:11 +0400

    refactor: use prisma with planetscale

---
+++ pages/api/category/index.ts
@@ -0,0 +1,43 @@
+import { NextApiHandler } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const handler: NextApiHandler = async (req, res) => {
+  const {
+    method,
+    body: { name, color },
+  } = req;
+
+  switch (method) {
+    case 'POST': {
+      if (!name || !color) {
+        return res
+          .status(400)
+          .json({ message: 'Missing required field(s)', success: false });
+      }
+      const newCategory = await prisma.category.create({
+        data: {
+          name,
+          color,
+        },
+      });
+      return res.status(201).json({ newCategory, success: true });
+    }
+    case 'GET': {
+      try {
+        const categories = await prisma.category.findMany();
+        return res.status(200).json(categories);
+      } catch (e) {
+        return res.status(500).json({ message: e.message, success: false });
+      }
+    }
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      return res
+        .status(405)
+        .json({ message: `Method ${method} Not Allowed`, success: false });
+  }
+};
+
+export default handler;
